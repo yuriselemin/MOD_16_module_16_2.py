a69496e74a59eb6d254c09f05874fb050664d49b
Selemin Yuriy <71262890+yuriselemin@users.noreply.github.com>
2024-10-14 18:09:24 +0300

    Update module_16_2.py

--- module_16_2.py
+++ module_16_2.py
@@ -19,3 +19,5 @@ async def user_page(user_id: Annotated[int, Path(ge=1, le=100, description="Ente
 async def user_info(username: Annotated[str, Path(min_length=5, max_length=20, description="Enter username", example='Vladimir')],
                     age: Annotated[int, Path(ge=18, le=120, description="Enter age", example='25')]) -> dict:
     return {"message": f"Информация о пользователе. Имя: {username}, Возраст: {age}"}
+
+
